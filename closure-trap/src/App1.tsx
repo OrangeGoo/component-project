960b8760015b1b143ff5b2f70a8ee442394b3ca4
OrangeGoo <gjc20021013@gmail.com>
2025-03-13 21:23:42 -0400

    feat: closure-trap

---
+++ closure-trap/src/App1.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(count);
+
+    setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  }, []);
+
+  return <div>{count}</div>;
+}
+
+export default App;
