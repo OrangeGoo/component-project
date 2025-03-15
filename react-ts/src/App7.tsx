1bb0055c1ee1a05435e8316606747f8df2b2b1f5
OrangeGoo <gjc20021013@gmail.com>
2025-03-14 21:38:28 -0400

    feat: react-ts

---
+++ react-ts/src/App7.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties, PropsWithChildren, ReactNode } from "react";
+
+type CccProps = PropsWithChildren<{
+  content: ReactNode;
+  color: CSSProperties["color"];
+  styles?: CSSProperties;
+}>;
+
+function Ccc(props: CccProps) {
+  return (
+    <div style={{ backgroundColor: props.color }}>
+      ccc,{props.content}
+      {props.children}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Ccc
+        content={<div>666</div>}
+        color="yellow"
+        styles={{
+          backgroundColor: "blue",
+        }}
+      >
+        <button>7777</button>
+      </Ccc>
+    </div>
+  );
+}
+
+export default App;
